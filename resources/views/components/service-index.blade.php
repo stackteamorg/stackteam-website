f431b6baa82a56b645244c7e6abef7c3e67acefb
Mahdi <homeily@gmail.com>
2023-08-19 10:14:08 +0330

    add service index widget

--- resources/views/components/service-index.blade.php
+++ resources/views/components/service-index.blade.php
@@ -1,3 +1,92 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+        <!--=====================================-->
+        <!--=        Service Area Start         =-->
+        <!--=====================================-->
+        <section class="section section-padding-2 bg-color-dark">
+            <div class="container">
+                <div class="section-heading heading-light-left">
+                    <span class="subtitle">What We Can Do For You</span>
+                    <h2 class="title">Services we can help you with</h2>
+                    <p class="opacity-50">Nulla facilisi. Nullam in magna id dolor
+                        blandit rutrum eget vulputate augue sed eu imperdiet.</p>
+                </div>
+                <div class="row">
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="100">
+                        <div class="services-grid active">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-1.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-design.html">Design</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-design.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="200">
+                        <div class="services-grid">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-2.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-development.html">Development</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-development.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="300">
+                        <div class="services-grid">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-3.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-marketing.html">Online marketing</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-marketing.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="100">
+                        <div class="services-grid">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-4.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-business.html">Business</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-business.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="200">
+                        <div class="services-grid">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-5.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-technology.html">Technology</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-technology.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6" data-sal="slide-up" data-sal-duration="800" data-sal-delay="300">
+                        <div class="services-grid">
+                            <div class="thumbnail">
+                                <img src="assets/media/icon/icon-6.png" alt="icon">
+                            </div>
+                            <div class="content">
+                                <h5 class="title"> <a href="service-content-strategy.html">Content strategy</a></h5>
+                                <p>Simply drag and drop photos and videos into your workspace to automatically add them to your Collab Cloud library.</p>
+                                <a href="service-content-strategy.html" class="more-btn">Find out more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ul class="list-unstyled shape-group-10">
+                <li class="shape shape-1"><img src="assets/media/others/line-9.png" alt="Circle"></li>
+                <li class="shape shape-2"><img src="assets/media/others/bubble-42.png" alt="Circle"></li>
+                <li class="shape shape-3"><img src="assets/media/others/bubble-43.png" alt="Circle"></li>
+            </ul>
+        </section>
