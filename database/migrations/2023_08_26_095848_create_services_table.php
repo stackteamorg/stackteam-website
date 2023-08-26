796fabb1401e90b88994fb7f2c4d067fab103740
Mahdi <homeily@gmail.com>
2023-08-26 14:24:38 +0330

    add model and migration for services

---
+++ database/migrations/2023_08_26_095848_create_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',25);
+            $table->string('title',255);
+            $table->string('subtitle',255);
+            $table->boolean('category_id',25);
+            $table->boolean('is_primary',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
