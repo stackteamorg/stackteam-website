aaa7f9749eac2ba524b44270ff94996442dab809
Mahdi <homeily@gmail.com>
2023-09-20 13:33:44 +0330

    change get in touch

--- resources/views/components/get-in-touch.blade.php
+++ resources/views/components/get-in-touch.blade.php
@@ -1,6 +1,6 @@
 <form>
     <div class="input-group">
-        <input type="email" class="form-control" placeholder="Email address">
-        <button class="subscribe-btn" type="submit">Subscribe</button>
+        <input type="email" class="form-control" placeholder="{{ __('content.get-in-touch.placeholder') }}">
+        <button class="subscribe-btn" type="submit">{{ __('content.get-in-touch.send') }}</button>
     </div>
 </form>
--- resources/views/pages/process.blade.php
+++ resources/views/pages/process.blade.php
@@ -1,4 +1,47 @@
 <x-website-layout>
     <x-process-banner />
-    <x-case-content />
+    
+    <section class="section-padding case-study-brief">
+            <div class="container">
+                <div class="row align-items-xl-center">
+                    <div class="col-lg-6" data-sal="slide-right" data-sal-duration="1000">
+                        <div class="case-study-featured-thumb">
+                            <img class="paralax-image" src="https://stackteam.org/assets/images/assets/feature-img-12.png" alt="Case Study">
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-lg-6 offset-xl-1" data-sal="slide-left" data-sal-duration="1000" data-sal-delay="200">
+                        <div class="case-study-featured">
+                            <div class="section-heading heading-left">
+                                <h2 class="title">Los Marineros</h2>
+                                <p>From the designers and engineers who are creating the next generation of web and mobile experiences, to anyone putting a website together for the first time. We provide elegant solutions that set new standards for online publishing.</p>
+                                <p>Digital technology has made our world more transparent and interconnected, posing new challenges and opportunities for every business. A holistic, user-centric perspective is what truly sets one apart.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-padding case-study-brief">
+            <div class="container">
+                <div class="row align-items-xl-center">
+                    <div class="col-xl-5 col-lg-6" data-sal="slide-right" data-sal-duration="1000" data-sal-delay="200">
+                        <div class="case-study-featured">
+                            <div class="section-heading heading-left">
+                                <h2 class="title">Los Marineros</h2>
+                                <p>From the designers and engineers who are creating the next generation of web and mobile experiences, to anyone putting a website together for the first time. We provide elegant solutions that set new standards for online publishing.</p>
+                                <p>Digital technology has made our world more transparent and interconnected, posing new challenges and opportunities for every business. A holistic, user-centric perspective is what truly sets one apart.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 offset-xl-1" data-sal="slide-left" data-sal-duration="1000">
+                        <div class="case-study-featured-thumb">
+                            <img class="paralax-image" src="https://stackteam.org/assets/images/assets/feature-img-13.png" alt="Case Study">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
 </x-website-layout>
